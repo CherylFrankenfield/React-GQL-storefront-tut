4ddb94925fd4d2b23ef749b0aee94c77b99f39b5
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2020-01-06 20:20:43 -0800

    Add app page and add nav, header, meta and page components

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,18 @@
+import Nav from './Nav';
+
+const Header = () => (
+    <div>
+        <div className="bar">
+            <a href="">sick fits</a>
+            <Nav />
+        </div>
+        <div className="sub-bar">
+            <p>Search</p>
+        </div>
+        <div>
+            Cart
+        </div>
+    </div>
+);
+
+export default Header;
